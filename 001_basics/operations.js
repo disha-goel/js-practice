904eb374001378c1a7a8846b3a132875f90c3391
disha-goel <dishagoel1205@gmail.com>
2025-10-24 20:47:33 +0530

    Basic Operations

---
+++ 001_basics/operations.js
@@ -0,0 +1,27 @@
+let num1=2;
+let num2=3;
+console.log(num1**num2);//2^3
+console.log("1"+2+2);//122
+/*
+ first value is a string JS converts everything to string and concatenates
+ first value is a number JS adds numbers until a string appears then concatenates as a string
+*/
+console.log(1+2+"2");//32
+// JS evaluates left to right
+console.log((3*4)+(5%2));
+//use parenthesis for better understanding 
+console.log(true);
+console.log(+true);//1
+console.log(+"");//1
+let score=100;
+console.log(score++);//100
+//post increment
+//Now score has been incremented
+console.log(score);//101
+
+
+
+
+
+
+
